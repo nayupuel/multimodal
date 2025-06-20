b2b6b7ca3978da323538520ca208763ca70deb17
nayupuel <b2baicc2025@gmail.com>
2025-06-20 10:48:31 +0900

    feat: 10-featureupdate

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    alert('버튼이 또 클릭되었습니다! (새로운 메시지 feature/add-branch)');
+    alert('버튼이 또 클릭되었습니다! (새로운 메시지10-featureupdateh)');
 });
